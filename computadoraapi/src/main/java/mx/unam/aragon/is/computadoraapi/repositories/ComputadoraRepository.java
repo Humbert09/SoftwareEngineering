3157c9a54751b97312397d476c8dbd0b432087fb
Humbert09 <113150226+Humbert09@users.noreply.github.com>
2025-05-06 00:03:17 -0600

    Aplicación creada con Spring

---
+++ computadoraapi/src/main/java/mx/unam/aragon/is/computadoraapi/repositories/ComputadoraRepository.java
@@ -0,0 +1,9 @@
+package mx.unam.aragon.is.computadoraapi.repositories;
+
+import mx.unam.aragon.is.computadoraapi.entities.Computadora;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ComputadoraRepository extends CrudRepository<Computadora, Long> {
+
+
+}
